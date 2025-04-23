3a9319618b1ef5e2a2dbf3a34c6538fbde176842
Trevor Jordan <tmjordan5@cps.edu>
2025-04-23 10:31:54 -0500

    xor nn

--- a8.py
+++ a8.py
@@ -2,6 +2,12 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
+
+xorn = NeuralNet(2, 3, 1)
+xorn.train(xor_training_data)
+print(xorn.test_with_expected(xor_training_data))
+
 
 
 
